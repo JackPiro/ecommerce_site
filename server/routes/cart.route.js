827ad2ba8be6eee29708372edc0ce7cf00bc444a
Noah Gardner <Noahgardner6@gmail.com>
2023-05-21 12:36:23 -0400

    setting up tailwind css to practice some front end development

--- server/routes/cart.route.js
+++
@@ -1,8 +0,0 @@
-const CartController = require('../controllers/cart.controller');
-module.exports = (app) => {
-  app.get('/api', CartController.findAllCarts);
-  app.get('/api/:id', CartController.findCart);
-  app.post('/api/cart', CartController.createCart);
-  app.put('/api/:id', CartController.updateCart);
-  app.delete('/api/:id', CartController.deleteCart);
-}
